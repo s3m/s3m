c02826b488101655a857ed93418b3802e1fbfc47
nbari <nbari@tequila.io>
2020-08-27 10:39:34 +0200

    todo #17

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 0.3.0
+## [Unreleased]
 * Using [blake3](https://crates.io/crates/blake3), for creating the file checksum, thanks @oconnor663
 * Using [sled](http://sled.rs/) with [serde_cbor](https://crates.io/crates/serde_cbor) to keep track of the uploaded files. thanks @spacejam, @D1plo1d
 * Multipart uploads can be resumed and is the default behavior.
--- src/s3m/stream.rs
+++ src/s3m/stream.rs
@@ -1,32 +1,13 @@
 use anyhow::Result;
 use futures::stream::TryStreamExt;
 use tokio::io::stdin;
-use tokio::io::BufReader;
 use tokio::prelude::*;
 use tokio_util::codec::{BytesCodec, FramedRead};
 
-const BUFFER_SIZE: usize = 536_870_912;
+const BUFFER_SIZE: usize = 1024 * 1024 * 5;
 
 // https://stackoverflow.com/q/41069865/1135424
 // https://users.rust-lang.org/t/reading-from-stdin-performance/2025
 pub async fn dispatcher() -> Result<()> {
-    let mut i: usize = 0;
-    let mut size: usize = 0;
-    let mut stream = FramedRead::new(stdin(), BytesCodec::new());
-    loop {
-        let mut in_memory_part = Vec::with_capacity(24);
-        while let Some(bytes) = stream.try_next().await? {
-            size += bytes.len();
-            in_memory_part.write_all(&bytes).await?;
-            if size % BUFFER_SIZE == 0 {
-                i += 1;
-                break;
-            }
-        }
-        println!("size: {}, i: {}", size, i);
-        if stream.try_next().await?.is_none() {
-            break;
-        }
-    }
-    Ok(())
+    todo!();
 }
