7718109aec86fd0a3b84924312e2fa2d66f657e6
nbari <nbari@tequila.io>
2020-08-27 10:46:45 +0200

    clippy 🍗

--- src/s3m/stream.rs
+++ src/s3m/stream.rs
@@ -1,10 +1,4 @@
 use anyhow::Result;
-use futures::stream::TryStreamExt;
-use tokio::io::stdin;
-use tokio::prelude::*;
-use tokio_util::codec::{BytesCodec, FramedRead};
-
-const BUFFER_SIZE: usize = 1024 * 1024 * 5;
 
 // https://stackoverflow.com/q/41069865/1135424
 // https://users.rust-lang.org/t/reading-from-stdin-performance/2025
